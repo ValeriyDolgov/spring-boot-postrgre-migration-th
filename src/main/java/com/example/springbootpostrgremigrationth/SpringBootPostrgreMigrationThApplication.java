f744f9d08fde9d17333eafa51955cd0f7da954d3
ValeriyDolgov <dolgov.valerka@mail.ru>
2022-10-30 14:47:22 +0600

    Empty project

---
+++ src/main/java/com/example/springbootpostrgremigrationth/SpringBootPostrgreMigrationThApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootpostrgremigrationth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPostrgreMigrationThApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootPostrgreMigrationThApplication.class, args);
+    }
+
+}
